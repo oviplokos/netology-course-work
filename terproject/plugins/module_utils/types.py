20ac772b4901bffc2b2cc4bfee544a8c980fbe5b
oviplokos <81755488+oviplokos@users.noreply.github.com>
2024-06-17 23:00:56 +0300

    Delete terproject/plugins directory

--- terproject/plugins/module_utils/types.py
+++
@@ -1,8 +0,0 @@
-from typing import Callable, Dict, List, Tuple, Union
-
-AnyJsonType = Union[Dict[str, "AnyJsonType"], List["AnyJsonType"], str, int, float, bool, None]
-TJsonObject = Dict[str, AnyJsonType]
-TJsonList = List[AnyJsonType]
-TJsonBareValue = Union[str, int, float, bool, None]
-
-AnsibleRunCommandType = Callable[..., Tuple[int, str, str]]
